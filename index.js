67d025d6e7b1d7b58525b74061e52e07194d94df
Shahabas Abdul Hameed <96893016+S488U@users.noreply.github.com>
2024-08-17 21:14:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,55 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path'; // Import the path module
 
 const port = 3500;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
+
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
+// Set EJS as the view engine
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    errorText : "",
-    dataString : "",
-    getText : ""
+app.get('/', (req, res) => {
+  res.render('index', {
+    errorText: '',
+    dataString: '',
+    getText: '',
   });
 });
 
-app.post("/submit", (req, res) => {
-  if (req.body.stringData === null || req.body.stringData === "") {
-    res.render("index.ejs", {
-      errorText: "Enter something to capitalize",
+app.post('/submit', (req, res) => {
+  if (req.body.stringData === null || req.body.stringData === '') {
+    res.render('index', {
+      errorText: 'Enter something to capitalize',
     });
   } else {
     let input = req.body.stringData;
 
     function capitalize(e) {
       let itemArray = [];
-      const newArray = e.split(" ");
+      const newArray = e.split(' ');
       newArray.forEach((item) => {
         var a = item.charAt(0).toUpperCase();
         var itemLength = item.length;
         var itemSliced = item.slice(1, itemLength);
-        var b = itemSliced != null ? itemSliced.toLowerCase() : "";
+        var b = itemSliced != null ? itemSliced.toLowerCase() : '';
         var itemJoined = a.concat(b);
         itemArray.push(itemJoined);
       });
 
-      return itemArray.join(" ");
+      return itemArray.join(' ');
     }
 
     const result = capitalize(input);
 
-    res.render("index.ejs", {
-      getText : input,
-      dataString : result,
-      errorText : ""
+    res.render('index', {
+      getText: input,
+      dataString: result,
+      errorText: '',
     });
   }
 });
